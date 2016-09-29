03b4aea5febf45e870e0417d64d5c5f53d0bebb5
fdovero <fdovero@vandoren.fr>
2016-09-29 08:20:24 +0200

    new descent.js exercice

--- solo/easy/javascript/the_descent.js
+++ solo/easy/javascript/the_descent.js
@@ -3,22 +3,12 @@ Array.prototype.maxIndex = function() {
 };
 
 // game loop
-while (true) {
-    var inputs = readline().split(' ');
-    var SX = parseInt(inputs[0]);
-    var SY = parseInt(inputs[1]);
-    var MH = new Array();
-
+while (true) { 
+    var MH = [];
+    
     for (var i = 0; i < 8; i++) {
         MH[i] = (parseInt(readline())); // represents the height of one mountain, from 9 to 0. Mountain heights are provided from left to right.
     }
 
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-
-    if (SX == MH.maxIndex()) {
-        print('FIRE');
-    } else {
-        print('HOLD'); // either:  FIRE (ship is firing its phase cannons) or HOLD (ship is not firing).
-    }
+    print(MH.maxIndex());
 }
